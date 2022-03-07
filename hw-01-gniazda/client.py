abc3630114e360fda754b4778b7aab3188cc456d
mhawryluk <marcinhawrylukxx@gmail.com>
2022-03-07 01:22:15 +0100

    Add receiving message in chunks

--- hw-01-gniazda/client.py
+++ hw-01-gniazda/client.py
@@ -3,26 +3,40 @@ from threading import Thread
 
 
 server_ip = "127.0.0.1"
-client_ip = "127.0.0.1"
-server_port = 9002
+server_port = 9018
 
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+running = True
 
 
 def send_message():
-    while True:
-        command = input('>> ').strip()
+    try:
+        while running:
+            command = input('>> ').strip()
 
-        if command.lower() == 't':
-            message = input('message: ')
-            send_tcp(message)
+            if command.lower() == 't':
+                message = input('message: ')
+                send_tcp(message+'\n')
 
-        elif command.lower() == 'u':
-            pass
-        elif command.lower() == 'm':
-            pass
-        else:
-            print('Unrecognized command: ', command)
+            elif command.lower() == 'u':
+                pass
+            elif command.lower() == 'm':
+                pass
+            else:
+                print('Unrecognized command: ', command)
+    except:
+        pass
+
+
+def listen_tcp():
+
+    while running:
+        message = tcp_socket.recv(5)
+
+        if message == b'':
+            raise RuntimeError("socket connection broken")
+
+        print(message.decode())
 
 
 def send_tcp(message):
@@ -31,29 +45,22 @@ def send_tcp(message):
     while total_sent < len(message):
         sent = tcp_socket.send(message[total_sent:].encode())
         if sent == 0:
-            raise RuntimeError("socket connection broken")
+            print("socket connection broken")
+            return
         total_sent = total_sent + sent
 
 
-def listen_tcp():
-
-    while True:
-        message = tcp_socket.recv(1024)
-
-        if message == b'':
-            raise RuntimeError("socket connection broken")
-
-        print(message.decode())
-
-
 if __name__ == '__main__':
-    tcp_socket.connect((server_ip, server_port))
+    try:
+        tcp_socket.connect((server_ip, server_port))
 
-    listen_thread = Thread(target=listen_tcp, daemon=True)
-    listen_thread.start()
+        listen_thread = Thread(target=listen_tcp, daemon=True)
+        listen_thread.start()
 
-    send_thread = Thread(target=send_message, daemon=True)
-    send_thread.start()
+        send_message()
 
-    listen_thread.join()
-    send_thread.join()
+    except:
+        running = False
+
+    finally:
+        tcp_socket.close()
--- hw-01-gniazda/server.py
+++ hw-01-gniazda/server.py
@@ -2,7 +2,7 @@ import socket
 from threading import Thread
 
 server_ip = "127.0.0.1"
-server_port = 9002
+server_port = 9018
 
 clients = []
 current_id = 0
@@ -18,30 +18,64 @@ class Client(Thread):
         self.id = current_id
         current_id += 1
         self.socket = socket
+        self.running = True
 
     def run(self):
-        while True:
+        buffer_message = ''
+        while self.running:
             message = self.socket.recv(5)
+
             if message == b'':
+                self.socket.close()
                 break
-            print(message.decode())
-            for client in clients:
-                if client != self:
-                    client.socket.send(message)
+
+            buffer_message += message.decode()
+
+            while True:
+                newline_index = buffer_message.find('\n')
+
+                if newline_index != -1:
+                    for client in clients:
+                        if client != self:
+                            self.send_tcp(
+                                client.socket, f'#{client.id}: {buffer_message[:newline_index+1]}\n')
+
+                    buffer_message = '' if newline_index == len(
+                        buffer_message) - 1 else buffer_message[newline_index+1:]
+                else:
+                    break
+
+    def send_tcp(self, socket, message):
+        total_sent = 0
+
+        while total_sent < len(message):
+            sent = socket.send(message[total_sent:].encode())
+            if sent == 0:
+                raise Exception("socket connection broken")
+            total_sent = total_sent + sent
 
 
 def listen_connections():
     tcp_socket.listen()
-    while True:
-        client_socket, address = tcp_socket.accept()
-        client_thread = Client(client_socket)
-        clients.append(client_thread)
-        client_thread.start()
+    try:
+        while True:
+            client_socket, address = tcp_socket.accept()
+            client_thread = Client(client_socket)
+            clients.append(client_thread)
+            client_thread.start()
+    except:
+        tcp_socket.close()
+        print('listen closed')
 
 
 if __name__ == '__main__':
     try:
         listen_connections()
     except:
+        for client in clients:
+            client.running = False
+        for client in clients:
+            client.join()
+
         tcp_socket.close()
-        print('closed')
+        print('main closed')
