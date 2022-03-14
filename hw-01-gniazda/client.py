d8e16a55fdb1d2c3ed9e87e9035b06a27d970d14
mhawryluk <marcinhawrylukxx@gmail.com>
2022-03-14 02:20:32 +0100

    Hw 1 done

--- hw-01-gniazda/client.py
+++ hw-01-gniazda/client.py
@@ -12,6 +12,59 @@ nick: str = ''
 running: bool = True
 
 
+def listen_tcp():
+    try:
+        for message in tcp_receive(tcp_socket):
+            if message is None:
+                dispose()
+                return
+            print(message)
+            print('>> ', end='')
+    except (KeyboardInterrupt, OSError):
+        pass
+    finally:
+        dispose()
+
+
+def listen_udp():
+    try:
+        while running:
+            message, address = udp_socket.recvfrom(1000)
+            print(message.decode())
+            print('>> ', end='')
+
+    except (KeyboardInterrupt, OSError):
+        pass
+    finally:
+        dispose()
+
+
+def listen_multicast():
+    global running
+    try:
+        while running:
+            message, address = multicast_socket.recvfrom(1000)
+            if address[1] != udp_socket.getsockname()[1]:
+                print(message.decode())
+                print('>> ', end='')
+    except (KeyboardInterrupt, OSError):
+        pass
+    finally:
+        dispose()
+
+
+def send_tcp(message: str):
+    tcp_socket.sendall(message.encode())
+
+
+def send_udp(message: str):
+    udp_socket.sendto(message.encode(), (server_ip, server_port))
+
+
+def send_multicast(message: str):
+    udp_socket.sendto(message.encode(), (multicast_ip, multicast_port))
+
+
 def send_message():
     global running
     try:
@@ -42,55 +95,6 @@ def send_message():
         dispose()
 
 
-def listen_tcp():
-    try:
-        for message in tcp_receive(tcp_socket):
-            if message is None:
-                dispose()
-                return
-            print(message)
-            print('>> ', end='')
-    except (KeyboardInterrupt, OSError):
-        pass
-    finally:
-        dispose()
-
-
-def listen_udp():
-    try:
-        while running:
-            message, address = udp_socket.recvfrom(1000)
-            print(message.decode())
-            print('>> ', end='')
-
-    except (KeyboardInterrupt, OSError):
-        dispose()
-
-
-def listen_multicast():
-    global running
-    try:
-        while running:
-            message, address = multicast_socket.recvfrom(1000)
-            if address[1] != udp_socket.getsockname()[1]:
-                print(message.decode())
-                print('>> ', end='')
-    except (KeyboardInterrupt, OSError):
-        dispose()
-
-
-def send_tcp(message: str):
-    tcp_socket.sendall(message.encode())
-
-
-def send_udp(message: str):
-    udp_socket.sendto(message.encode(), (server_ip, server_port))
-
-
-def send_multicast(message: str):
-    udp_socket.sendto(message.encode(), (multicast_ip, multicast_port))
-
-
 def dispose():
     global running
     running = False
@@ -102,30 +106,31 @@ def dispose():
 
 if __name__ == '__main__':
     try:
+        # connect to tcp server
         tcp_socket.connect((server_ip, server_port))
         _, port = tcp_socket.getsockname()
         udp_socket.bind(('', port))
 
+        # choose nick
         nick = input('nick: ')
-        tcp_socket.sendall((nick+'\n').encode())
+        tcp_socket.sendall((nick + '\n').encode())
 
-        multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        membership = socket.inet_aton(multicast_ip) + socket.inet_aton("0.0.0.0")
-        multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
+        # initialize multicast channel
+        ip_mreq = socket.inet_aton(multicast_ip) + socket.inet_aton('0.0.0.0')
+        multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, ip_mreq)
         multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
         multicast_socket.bind(('', multicast_port))
 
+        # start threads
         tcp_thread = Thread(target=listen_tcp, daemon=True)
         udp_thread = Thread(target=listen_udp, daemon=True)
         multicast_thread = Thread(target=listen_multicast, daemon=True)
-
         tcp_thread.start()
         udp_thread.start()
         multicast_thread.start()
 
+        # enter send message prompt
         send_message()
 
     except KeyboardInterrupt:
