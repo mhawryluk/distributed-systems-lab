23674345bb481b7df917ab16e0c0baef610f6c18
mhawryluk <marcinhawrylukxx@gmail.com>
2022-03-18 14:29:41 +0100

    Add fetching lyrics

--- hw-02-rest/server.py
+++ hw-02-rest/server.py
@@ -1,15 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, Response
 from flask_cors import CORS
-import requests
+from requests import get, Response as response_t
+
 
 app = Flask(__name__)
 CORS(app)
 
 
+def get_lyrics(artist: str, title: str):
+    response: response_t = get(f'https://api.lyrics.ovh/v1/{artist}/{title}')
+    if response.status_code == 200:
+        return response.json()['lyrics'].replace('\n', '<br>').replace('\r', '<br>')
+
+
 @app.route('/service', methods=['GET'])
 def service():
-    argument = request.args.get('arg', '')
-    return f'<html><body><h1>{argument}</h1></body></html>'
+
+    artist: str = request.args.get('artist', '')
+    title: str = request.args.get('title', '')
+
+    if not (artist and title):
+        return Response(f'<p>wrong arguments (artist: {artist}, title: {title})</p>', status=400, mimetype='text/html')
+
+    html = f'<h1>{artist} - {title}</h1>'
+
+    lyrics = get_lyrics(artist, title)
+    if lyrics:
+        html += f'<h2>Lyrics</h2> <p>{lyrics}</p>'
+
+    return Response(html, status=200, mimetype='text/html')
 
 
 if __name__ == '__main__':
