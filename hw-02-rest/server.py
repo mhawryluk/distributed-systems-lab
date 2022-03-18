3bdcf974905fd0150c82c7a44fdfca8ea99e3306
mhawryluk <marcinhawrylukxx@gmail.com>
2022-03-19 00:07:46 +0100

    Add requesting artist info and recs

--- hw-02-rest/server.py
+++ hw-02-rest/server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, Response
 from flask_cors import CORS
 from requests import get, request as send_request, Response as response_t
 from secret import translator_headers, translator_url, translator_querystring
-from typing import List
+from typing import List, Tuple
 from collections import Counter
 
 app = Flask(__name__)
@@ -37,6 +37,15 @@ def get_lyrics(artist: str, title: str) -> List[str]:
         return response.json()['lyrics'].replace('\n\n', '|').replace('\r\n', '|').split('|')
 
 
+def get_artist_info_recs(artist: str) -> Tuple[str, List[str]]:
+    response: response_t = get(f'https://tastedive.com/api/similar?q={artist}&info=1')
+    if response.status_code == 200:
+        response_json = response.json()['Similar']
+        return response_json['Info'][0]['wTeaser'], [rec['Name'] for rec in response_json['Results'][:5]]
+
+    return '', []
+
+
 @app.route('/service', methods=['GET'])
 def service():
     artist: str = request.args.get('artist', '')
@@ -51,12 +60,18 @@ def service():
     if lyrics:
         html += f'<h2>Lyrics</h2> <p>{"<br>".join(lyrics)}</p>'
 
-        # lyric_stats = get_lyric_stats(lyrics)
-        # html += f'<h2> Lyric word stats </h2><p>{"<br>".join(lyric_stats)}</p>'
+        lyric_stats = get_lyric_stats(lyrics)
+        html += f'<h2> Lyric word stats </h2><p>{"<br>".join(lyric_stats)}</p>'
 
-        # translated_lyrics = translate(lyrics[:5])
-        # if translated_lyrics:
-        #     html += f'<h2>Translated lyrics</h2> <p>{"<br>".join(translated_lyrics)}(...)</p>'
+        translated_lyrics = translate(lyrics[:5])
+        if translated_lyrics:
+            html += f'<h2>Translated lyrics</h2> <p>{"<br>".join(translated_lyrics)}(...)</p>'
+
+    info, recs = get_artist_info_recs(artist)
+    if info:
+        html += f'<h2> Artist Info </h2> <p>{info}</p>'
+    if recs:
+        html += f'<h2> Similar to {artist} </h2> <p>{"<br>".join(recs)}</p>'
 
     return Response(html, status=200, mimetype='text/html')
 
