40f0697101560b2a51cbd40062c9beef30cb1ff7
mhawryluk <marcinhawrylukxx@gmail.com>
2022-03-20 00:06:11 +0100

    Make api requests asynchronous

--- hw-02-rest/server.py
+++ hw-02-rest/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, Response
 from flask_cors import CORS
-from requests import get, request as send_request, Response as response_t
+from requests import get, Response as response_t
 from secret import translator_headers, translator_url, translator_querystring
-from typing import List, Tuple
+from typing import List, Tuple, Optional, Any
 from collections import Counter
+import asyncio
+import aiohttp
 
 app = Flask(__name__)
 CORS(app)
@@ -13,37 +15,49 @@ def get_lyric_stats(lyrics: List[str]):
     return map(lambda item: f'{item[0]}: {item[1]}', Counter(' '.join(lyrics).lower().split(' ')).most_common(10))
 
 
-def translate(lyrics: List[str]):
-    translated = []
-    for text in lyrics:
-        response: response_t = send_request(
-            "POST",
+def get_translate_tasks(lyrics: List[str], session: aiohttp.ClientSession):
+    return [
+        session.post(
             translator_url,
             data="[{\"Text\": \"" + text + "\"}]",
             headers=translator_headers,
             params=translator_querystring
         )
 
-        if response.status_code == 200:
-            translated.append(response.json()[0]['translations'][0]['text'])
-
-    if translated:
-        return translated
+        for text in lyrics
+    ]
 
 
-def get_lyrics(artist: str, title: str) -> List[str]:
-    response: response_t = get(f'https://api.lyrics.ovh/v1/{artist}/{title}')
-    if response.status_code == 200:
-        return response.json()['lyrics'].replace('\n\n', '|').replace('\r\n', '|').split('|')
+async def get_translated_lyrics(lyrics: List[str]) -> List[str]:
+    translated = []
+    async with aiohttp.ClientSession() as session:
+        tasks = get_translate_tasks(lyrics, session)
+        responses = await asyncio.gather(*tasks)
+        for response in responses:
+            translated.append((await response.json())[0]['translations'][0]['text'])
+    return translated
 
 
-def get_artist_info_recs(artist: str) -> Tuple[str, List[str]]:
-    response: response_t = get(f'https://tastedive.com/api/similar?q={artist}&info=1')
-    if response.status_code == 200:
-        response_json = response.json()['Similar']
-        return response_json['Info'][0]['wTeaser'], [rec['Name'] for rec in response_json['Results'][:5]]
+async def get_lyrics_and_info(artist: str, title: str) -> tuple[Optional[List[str]], Optional[list[Any]]]:
+    async with aiohttp.ClientSession() as session:
+        tasks = [
+            session.get(f'https://api.lyrics.ovh/v1/{artist}/{title}'),
+            session.get(f'https://tastedive.com/api/similar?q={artist}&info=1')
+        ]
 
-    return '', []
+        responses = await asyncio.gather(*tasks)
+
+        lyrics = None
+        info = (None, None)
+
+        if responses[0].status == 200:
+            lyrics = (await responses[0].json())['lyrics'].replace('\n\n', '|').replace('\r\n', '|').split('|')
+
+        if responses[1].status == 200:
+            info_json = (await responses[1].json())['Similar']
+            info = [info_json['Info'][0]['wTeaser'], [rec['Name'] for rec in info_json['Results'][:5]]]
+
+    return lyrics, info
 
 
 @app.route('/service', methods=['GET'])
@@ -56,18 +70,18 @@ def service():
 
     html = f'<h1>{artist} - {title}</h1>'
 
-    lyrics = get_lyrics(artist, title)
+    lyrics, (info, recs) = asyncio.run(get_lyrics_and_info(artist, title))
+
     if lyrics:
         html += f'<h2>Lyrics</h2> <p>{"<br>".join(lyrics)}</p>'
 
         lyric_stats = get_lyric_stats(lyrics)
         html += f'<h2> Lyric word stats </h2><p>{"<br>".join(lyric_stats)}</p>'
 
-        translated_lyrics = translate(lyrics[:5])
+        translated_lyrics: List[str] = asyncio.run(get_translated_lyrics(lyrics[:5]))
         if translated_lyrics:
             html += f'<h2>Translated lyrics</h2> <p>{"<br>".join(translated_lyrics)}(...)</p>'
 
-    info, recs = get_artist_info_recs(artist)
     if info:
         html += f'<h2> Artist Info </h2> <p>{info}</p>'
     if recs:
