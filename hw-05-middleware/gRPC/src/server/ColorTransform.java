bb21c59eec0eef1f4f55567b4028b03dc6140ee1
mhawryluk <marcinhawrylukxx@gmail.com>
2022-05-29 15:04:59 +0200

    Delete unnecessary comments

--- hw-05-middleware/gRPC/src/server/ColorTransform.java
+++ hw-05-middleware/gRPC/src/server/ColorTransform.java
@@ -99,42 +99,4 @@ public class ColorTransform extends ColorTransformGrpc.ColorTransformImplBase {
 		responseObserver.onNext(result);
 		responseObserver.onCompleted();
 	}
-
-//	@Override
-//	public void add(Argument request,
-//			io.grpc.stub.StreamObserver<sr.grpc.gen.ArithmeticOpResult> responseObserver) {
-//		System.out.println("addRequest (" + request.getArg1() + ", " + request.getArg2() +")");
-//		int val = request.getArg1() + request.getArg2();
-//		ArithmeticOpResult result = ArithmeticOpResult.newBuilder().setRes(val).build();
-//		if(request.getArg1() > 100 && request.getArg2() > 100) try { Thread.sleep(5000); } catch(InterruptedException ex) { }
-//		responseObserver.onNext(result);
-//		responseObserver.onCompleted();
-//	}
-//
-//	@Override
-//	public void subtract(sr.grpc.gen.ArithmeticOpArguments request,
-//			io.grpc.stub.StreamObserver<sr.grpc.gen.ArithmeticOpResult> responseObserver)
-//	{
-//		System.out.println("subtractRequest (" + request.getArg1() + ", " + request.getArg2() +")");
-//
-//		responseObserver.onError(io.grpc.Status.INVALID_ARGUMENT.withDescription("Bad arguments").asRuntimeException());
-//
-//		int val = request.getArg1() - request.getArg2();
-//		ArithmeticOpResult result = ArithmeticOpResult.newBuilder().setRes(val).build();
-//		responseObserver.onNext(result);
-//		responseObserver.onCompleted();
-//	}
-//
-//	@Override
-//	public void multiply(sr.grpc.gen.MultiplyArgs request, io.grpc.stub.StreamObserver<sr.grpc.gen.ArithmeticOpResult> responseObserver) {
-//		int val = 1;
-//		for (int num : request.getNumbersList()) {
-//			val *= num;
-//		}
-//
-//		ArithmeticOpResult result = ArithmeticOpResult.newBuilder().setRes(val).build();
-//		responseObserver.onNext(result);
-//		responseObserver.onCompleted();
-//	}
-
 }
