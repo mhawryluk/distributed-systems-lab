b6e91a2cc423caf159a0ea535f727abf766a5b04
mhawryluk <marcinhawrylukxx@gmail.com>
2022-06-11 15:30:56 +0200

    Add opening and closing an app on node creation/deletion

--- hw-06-zookeeper/src/main/scala/NodeWatcher.scala
+++ hw-06-zookeeper/src/main/scala/NodeWatcher.scala
@@ -3,34 +3,41 @@ package agh.distributed
 import org.apache.zookeeper.{AddWatchMode, WatchedEvent, Watcher, ZooKeeper}
 import org.apache.zookeeper.Watcher.Event
 
-class ZWatcher(val program: String, val node: String) extends Watcher {
+import scala.sys.process.Process
+import scala.io.StdIn.readLine
 
-  val zk = new ZooKeeper("localhost:2184", 2000, this)
-  zk.addWatch(node, this, AddWatchMode.PERSISTENT)
+class NodeWatcher(val program: String, val node: String) extends Watcher {
+
+  private val client = new ZooKeeper("localhost:2184", 2000, this)
+  client.addWatch(node, this, AddWatchMode.PERSISTENT)
+
+  private val pb = Process(program)
+  var p: Option[Process] = None
 
   override def process(watchedEvent: WatchedEvent): Unit = {
-    println(watchedEvent.getState)
-
     watchedEvent.getType match {
       case Event.EventType.NodeCreated =>
         println(s"node ${watchedEvent.getPath} created")
+        p = Option(pb.run())
       case Event.EventType.NodeDeleted =>
         println(s"node ${watchedEvent.getPath} removed")
+        p.foreach(_.destroy)
       case other =>
         println(other)
     }
   }
 }
 
-object ZWatcher {
+object NodeWatcher {
   def main(args: Array[String]): Unit = {
 
     val program = args(0)
-    val zWatcher = new ZWatcher(program, "/z")
+    val zWatcher = new NodeWatcher(program, "/z")
 
     new Thread(() => {
       while (true) {
-
+        readLine()
+        println("hi")
       }
     }).start()
   }
