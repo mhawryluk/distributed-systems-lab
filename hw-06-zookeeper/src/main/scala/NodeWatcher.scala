86a59729d1644dbd739603834e5babf2e7910909
mhawryluk <marcinhawrylukxx@gmail.com>
2022-06-11 16:01:15 +0200

    Add printing children count

--- hw-06-zookeeper/src/main/scala/NodeWatcher.scala
+++ hw-06-zookeeper/src/main/scala/NodeWatcher.scala
@@ -9,7 +9,7 @@ import scala.io.StdIn.readLine
 class NodeWatcher(val program: String, val node: String) extends Watcher {
 
   private val client = new ZooKeeper("localhost:2184", 2000, this)
-  client.addWatch(node, this, AddWatchMode.PERSISTENT)
+  client.addWatch(node, this, AddWatchMode.PERSISTENT_RECURSIVE)
 
   private val pb = Process(program)
   var p: Option[Process] = None
@@ -18,10 +18,21 @@ class NodeWatcher(val program: String, val node: String) extends Watcher {
     watchedEvent.getType match {
       case Event.EventType.NodeCreated =>
         println(s"node ${watchedEvent.getPath} created")
-        p = Option(pb.run())
+        watchedEvent.getPath match {
+          case `node` => p = Option(pb.run())
+          case path if path startsWith node =>
+            println(s"All children count: ${client getAllChildrenNumber node}")
+          case other => println(s"unrecognized node: $other")
+        }
+
       case Event.EventType.NodeDeleted =>
-        println(s"node ${watchedEvent.getPath} removed")
-        p.foreach(_.destroy)
+        if (watchedEvent.getPath == node) {
+          println(s"node $node removed")
+          p.foreach(_.destroy)
+        }
+
+      case Event.EventType.None =>
+
       case other =>
         println(other)
     }
@@ -32,7 +43,7 @@ object NodeWatcher {
   def main(args: Array[String]): Unit = {
 
     val program = args(0)
-    val zWatcher = new NodeWatcher(program, "/z")
+    new NodeWatcher(program, "/z")
 
     new Thread(() => {
       while (true) {
